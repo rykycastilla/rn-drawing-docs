d54eb116796f611103bf3847d265342565f284f9
Orestes Ricardo Castilla Escalona <rcastilla744@gmail.com>
2025-05-06 16:26:08 -0400

    feat: Supporting english in page not found

--- src/pages/NotFound.tsx
+++ src/pages/NotFound.tsx
@@ -3,6 +3,7 @@ import { Button } from '@/libs/ui/button'
 import { Home } from 'lucide-react'
 import { Link } from 'react-router-dom'
 import { ReactElement } from 'react'
+import { useLanguage } from '@/hooks/language'
 
 const Error404 = (): ReactElement => {
   return (
@@ -13,12 +14,13 @@ const Error404 = (): ReactElement => {
 }
 
 const HomeButton = (): ReactElement => {
+  const { t } = useLanguage()
   return (
     <div className="flex justify-center mb-10 md:mb-20">
       <Button asChild size="default" className="bg-pink-500 hover:bg-pink-600 text-white">
         <Link to="/" className="flex items-center gap-2">
           <Home className="h-4 w-4 md:h-5 md:w-5" />
-          <span>Volver al inicio</span>
+          <span>{ t( 'come-home' ) }</span>
         </Link>
       </Button>
     </div>
@@ -26,19 +28,20 @@ const HomeButton = (): ReactElement => {
 }
 
 const NotFound = (): ReactElement => {
+  const { t } = useLanguage()
   return (
     <>
-      <Title>Page not found!</Title>
+      <Title>{ t( 'page-not-found-title' ) }</Title>
       <div className="error-page flex flex-col items-center justify-center pt-10 md:pt-20">
         <div className="container max-w-3xl text-center px-4">
           <div className="mb-6 md:mb-12">
             <Error404 />
           </div>
           <h2 className="text-2xl md:text-6xl font-bold tracking-tight text-gray-900 dark:text-white mb-3 md:mb-4">
-          ¡Ups! Página no encontrada
+            { t( 'page-not-found-alert' ) }
           </h2>
           <p className="text-base md:text-xl text-gray-600 dark:text-gray-300 max-w-2xl mx-auto mb-6 md:mb-8">
-          Parece que has intentado dibujar fuera del lienzo. La página que buscas no existe o ha sido movida.
+            { t( 'page-not-found' ) }
           </p>
           <HomeButton />
         </div>
