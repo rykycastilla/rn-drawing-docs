1e8bba356c3bb13857aa09ff19377ea62f1fe5c3
Orestes Ricardo Castilla Escalona <rcastilla744@gmail.com>
2025-05-10 19:22:42 -0400

    feat: Adding filler page

--- src/pages/Docs/data/index.ts
+++ src/pages/Docs/data/index.ts
@@ -1,9 +1,20 @@
 import dotTools from './dot_tools.json'
+import filler from './filler.json'
 import imageLayout from './image_layout.json'
 import installing from './installing.json'
 import gettingStarted from './getting_started.json'
 import grid from './grid.json'
+import spray from './spray.json'
 import strokeTools from './stroke_tools.json'
 import { PageStructure } from '../models'
 
-export const data: PageStructure[] = [ installing, gettingStarted, imageLayout, grid, strokeTools, dotTools ]
+export const data: PageStructure[] = [
+  installing,
+  gettingStarted,
+  imageLayout,
+  grid,
+  strokeTools,
+  dotTools,
+  spray,
+  filler,
+]
