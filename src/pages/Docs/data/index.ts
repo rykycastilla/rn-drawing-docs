fa7cbea9a48a352bcb12ae17e036435fa81d5058
Orestes Ricardo Castilla Escalona <rcastilla744@gmail.com>
2025-05-10 12:50:04 -0400

    feat: Adding image & layout page

--- src/pages/Docs/data/index.ts
+++ src/pages/Docs/data/index.ts
@@ -1,5 +1,6 @@
+import imageLayout from './image_layout.json'
 import installing from './installing.json'
 import gettingStarted from './getting_started.json'
 import { PageStructure } from '../models'
 
-export const data: PageStructure[] = [ installing, gettingStarted ]
+export const data: PageStructure[] = [ installing, gettingStarted, imageLayout ]
