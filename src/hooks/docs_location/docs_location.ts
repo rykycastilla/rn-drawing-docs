5f91972b6859876836f70ef4ec27de4d876a1c7c
Orestes Ricardo Castilla Escalona <rcastilla744@gmail.com>
2025-05-13 10:33:13 -0400

    doc: docs_location hook

--- src/hooks/docs_location/docs_location.ts
+++ src/hooks/docs_location/docs_location.ts
@@ -2,6 +2,10 @@ import { NotFoundDocContext } from './context'
 import { useContext } from 'react'
 import { useLocation } from 'react-router-dom'
 
+/**
+ * Indicates that the current route is a "docs" child.
+ * Only it will be ignored if you are rendering a page marked as "not found" by `useNotFound`
+ */
 export function useDocsLocation(): boolean {
   const contextResult: NotFoundDocContext | null = useContext( NotFoundDocContext )
   const { isNotFound = false } = contextResult ?? {}
--- src/hooks/docs_location/not_found.ts
+++ src/hooks/docs_location/not_found.ts
@@ -1,6 +1,10 @@
 import { NotFoundDocContext } from './context'
 import { useContext, useEffect } from 'react'
 
+/**
+ * Using to indicates to `useDocsLocation` that the route rendering the parent component
+ * is a "not found page" instead of a regular "docs"
+ */
 export function useNotFound() {
   const { setIsNotFound } = useContext( NotFoundDocContext )!
   useEffect( () => {
