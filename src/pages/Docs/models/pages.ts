da55388387ba0f227d9d5997537477da89744ba9
Orestes Ricardo Castilla Escalona <rcastilla744@gmail.com>
2025-08-10 20:35:28 -0400

    feat: Adding background color docs. Grid page was restructured to Artboard to accepts it

--- src/pages/Docs/models/pages.ts
+++ src/pages/Docs/models/pages.ts
@@ -4,7 +4,7 @@ export const pages: Page[] = [
   new Page( 'installing' ),
   new Page( 'getting-started' ),
   new Page( 'image-layout' ),
-  new Page( 'grid' ),
+  new Page( 'artboard' ),
   new Page( 'stroke-tools' ),
   new Page( 'dot-tools' ),
   new Page( 'spray' ),
