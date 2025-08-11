43f5dd8ce9217b1775782b3c61e4c16000287256
Orestes Ricardo Castilla Escalona <rcastilla744@gmail.com>
2025-08-11 18:42:17 -0400

    fix: Solving bakdrop-overlay bug of mobile menu. touch out exit event unreachable (iOS). Invisible overlay (Rest of platforms)

--- src/components/MobileMenu/components/Panel.tsx
+++ src/components/MobileMenu/components/Panel.tsx
@@ -10,18 +10,12 @@ const Panel = ( props:PanelProps ): ReactElement => {
   const { isOpen, close, children } = props
   return (
     <>
-      { /* BackDrop */ }
-      {
-        isOpen &&
-        <div
-          className="fixed z-50 top-0 left-0 w-screen h-screen"
-          onClick={ close } />
-      }
       { /* Overlay */ }
       <div
-        className={`fixed inset-0 bg-black/50 z-50 transition-opacity duration-300 ${
+        className={`fixed w-screen h-screen inset-0 bg-black/50 z-50 transition-opacity duration-300 ${
           isOpen ? 'opacity-100' : 'opacity-0 pointer-events-none'
-        }`} />
+        }`}
+        onClick={ close } />
       { /* Panel */ }
       <div
         className={`fixed top-0 left-0 right-0 bg-background border-b z-50 shadow-lg transition-transform duration-300 transform rounded-b-lg max-h-[100vh] overflow-scroll ${
